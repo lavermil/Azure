a0b7fbec7838520db0cd300bfe9a01f0956b6e68
lavermil <github@gheek.net>
2021-03-16 14:33:18 +0700

    Update ConfigurationDiskinitialization.ps1
    Add a lot of error checking and had to move the reserved DVD drive to a new letter since it defaults to next available after Temp. DVD is used in Azure in rare cases so don't disable it.

--- DSC/ConfigurationDiskinitialization.ps1
+++ DSC/ConfigurationDiskinitialization.ps1
@@ -7,10 +7,17 @@ Configuration Diskinitialization
             # Start logging the actions 
             Start-Transcript -Path C:\Temp\Diskinitlog.txt -Append -Force
 
+            # Move DVD drive letter since it is there by default and should only be needed at provisioning, but be safe and keep it reserved.
+            # https://github.com/MicrosoftDocs/azure-docs/issues/27776
+            # Move CD-ROM drive to Q for now:
+            "Moving CD-ROM drive to Q:.."
+            Get-WmiObject -Class Win32_volume -Filter 'DriveType=5' | Select-Object -First 1 | Set-WmiInstance -Arguments @{DriveLetter='Q:'}
+
             # Get list of Disks that are not initialized
             $disks = Get-Disk | Where-Object partitionstyle -eq 'raw' | Sort-Object number
 
-            #DriveLabel,DriveLetter,Lun
+            # DriveLabel,DriveLetter,Lun
+            # Todo: Allow this to be passed as argument
             $driveparams = @('SQLDATA,E,2','SQLLOG,L,3')
  
             "Formatting disks.."
@@ -19,20 +26,37 @@ Configuration Diskinitialization
                     $driveLabel = $driveparam.split(",")[0]
                     $driveLetter = $driveparam.split(",")[1]
                     $driveLun = $driveparam.split(",")[2]
+                    $drivePathLetter = (-join($driveLetter,":\",$driveLabel))
                     if ( $disk.Number -eq $driveLun ) {
+                    "Working on: driveLabel: $(($driveLabel)) driveLetter: $(($driveLetter)) driveLUN: $(($driveLun))"
                         $disk |
                         Initialize-Disk -PartitionStyle GPT -PassThru |
                         New-Partition -UseMaximumSize -DriveLetter $driveLetter |
                         Format-Volume -FileSystem NTFS -NewFileSystemLabel "$driveLabel" -Confirm:$false -AllocationUnitSize 65536 -Force
                         if ( $? ) {
-                            "Created: driveLabel: $(($driveLabel)) driveLetter: $(($driveLetter)) driveLUN: $(($driveLun))"
-                            md -Path "$(($driveLetter:\$driveLabel))"
+                            md -Path $drivePathLetter
+                            $retstatus = $?
+                            $verifypathdir = Test-Path $drivePathLetter
+                            if ( $retstatus -and $verifypathdir ) {
+                                "  Created: driveLabel: $(($driveLabel)) driveLetter: $(($driveLetter)) driveLUN: $(($driveLun))"
+                            }
                         }
                     }
                 }
             }
             # Create the D:SQLDATA if it isn't created
-            md -Path "D:\SQLDATA"
+            $drivePathLetter = "D:\SQLDATA"
+            $verifypathdir = Test-Path $drivePathLetter
+            if ( $verifypathdir -eq $false ) {
+                md -Path $drivePathLetter
+                $retstatus = $?
+                $verifypathdir = Test-Path $drivePathLetter
+                if ( $retstatus -and $verifypathdir ) {
+                    "  Created: driveLabel: $(($driveLabel)) driveLetter: $(($driveLetter)) driveLUN: $(($driveLun))"
+                }
+            } else {
+                "  Already Exists: driveLabel: $(($driveLabel)) driveLetter: $(($driveLetter)) driveLUN: $(($driveLun))"
+            }
             Stop-Transcript
         }
 
